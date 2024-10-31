812155761f3915704fe06402712a9421362e332a
skiparb <skiparb@gmail.com>
2024-10-31 11:25:59 +0100

    fiku miku na patyku jakies

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        MyList<Integer> list = new MyList<>();
+        System.out.println("Podaj wartosci do wpisania na liste. wpisanie -1 powoduje koniec wczytywania");
+        Scanner scanner = new Scanner(System.in);
+        Integer value;
+        while(true) {
+            value = scanner.nextInt();
+            if (value == -1){
+                break;
+            }
+            list.addElement(value);
+        }
+        list.display();
+        System.out.println();
+        System.out.println("-----------------------------");
+        list.removeElement();
+        list.display();
+        System.out.println();
+        System.out.println("-----------------------------");
+        list.getElement(5);
+        System.out.println();
     }
 }
--- src/MyList.java
+++ src/MyList.java
@@ -2,6 +2,54 @@ public class MyList<T> {
     private Node head;
 
     public void addElement(T value){
+        Node<T> node = new Node<>(value,null);
+        if(head == null){
+            head = node;
+        }
+        else{
+            Node<T> Element = head;
+            while (Element.next != null){
+                Element = Element.next;
+            }
+            Element.next = node;
+        }
+    }
 
+    // wyświetla wszystkie elementy listy
+    public void display() {
+        Node<T> node = head;
+        if(node != null){
+            while(node.next != null){
+                System.out.print(node.value);
+                node = node.next;
+            }
+        }
+    }
+    public void removeElement(){
+        Node<T> node = head;
+        if(node != null){
+            Node<T> prev = node;
+            while (node.next != null){
+                prev = node;
+                node = node.next;
+            }
+            if(prev != node){
+                prev.next = null;
+            }else{
+                head = null;
+            }
+        }
+    }
+    public Node<T> getElement(T value){
+        Node<T> node = head;
+        if(node != null){
+            while(node.next != null){
+                if(node.value.equals(value)){
+                    return node;
+                }
+                node = node.next;
+            }
+        }
+        return null;
     }
 }
